eccb4acab67f40d707f68b2ea3af55297cabdd26
Нина Карлова <nina-karlova@yandex.ru>
2023-03-06 19:38:41 +0300

    Пробная отправка черновика финального проекта

---
+++ yatube/posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self, title):
+        return self.title
+
+class Post(models.Model):
+    text = models.TextField('Содержание поста', blank=True, null=True)
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор поста'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+        verbose_name='Сообщество для публикации поста'
+    )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-pub_date',)
+
---
+++ yatube/posts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+NUM_ON_SCREEN = 10
+
+def index(request):
+    posts = Post.objects.all()[:NUM_ON_SCREEN]
+    context = { 'posts': posts, }    
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:NUM_ON_SCREEN]    
+    context = {
+        'group': group,
+        'posts': posts,
+    }    
+    return render(request, 'posts/group_list.html', context)
