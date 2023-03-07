96e18a517b272b7d7411af866c50cde94927d00d
Нина Карлова <nina-karlova@yandex.ru>
2023-03-07 18:27:12 +0300

    Исправления по ревью 1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,8 +8,12 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
-    def __str__(self, title):
+    def __str__(self):
         return self.title
 
 
@@ -30,7 +34,7 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Сообщество для публикации поста'
     )
-
+    
     class Meta:
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-NUM_ON_SCREEN = 10
+NUM_POSTS_ON_SCREEN = 10
 
 
 def index(request):
-    posts = Post.objects.all()[:NUM_ON_SCREEN]
+    posts = Post.objects.select_related('group').all()[:NUM_POSTS_ON_SCREEN]
     context = {'posts': posts, }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:NUM_ON_SCREEN]
+    posts = group.posts.all()[:NUM_POSTS_ON_SCREEN]
     context = {
         'group': group,
         'posts': posts,
